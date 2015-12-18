04bfcbae55a89df825e1b72fd62e1a782a138cfd
Kacper Walanus <kacper@walanus.com>
2015-12-18 21:10:28 +0100

    simple handling of :if and :unless options

--- lib/html5_validators/active_model/helper_methods.rb
+++ lib/html5_validators/active_model/helper_methods.rb
@@ -1,9 +1,18 @@
 module ActiveModel
   module Validations
     module HelperMethods
-      def attribute_required?(attribute)
-        self.validators.grep(PresenceValidator).any? do |v|
-          v.attributes.include?(attribute.to_sym) && (v.options.keys & [:if, :unless]).empty?
+      def attribute_required?(object, attribute)
+        validator = object.class.validators.grep(PresenceValidator).find { |v| v.attributes.include?(attribute.to_sym) }
+        return false unless validator
+
+        if cant_handle_conditions?(validator)
+          (validator.options.keys & [:if, :unless]).empty?
+        else
+          if_condition = if_condition(validator)
+          return object.send(if_condition) if can_handle_condition?(if_condition)
+
+          unless_condition = unless_condition(validator)
+          return !object.send(unless_condition) if can_handle_condition?(unless_condition)
         end
       end
 
@@ -29,6 +38,24 @@ module ActiveModel
         self.validators.grep(NumericalityValidator).select {|v|
           v.attributes.include?(attribute.to_sym) && (v.options.keys & [:greater_than, :greater_than_or_equal_to]).any? && (v.options.keys & [:if, :unless, :allow_nil, :allow_blank]).empty?
         }.map {|v| v.options.slice(:greater_than, :greater_than_or_equal_to)}.map(&:values).flatten.min
+      end
+
+      private
+
+      def unless_condition(validator)
+        validator.options[:unless]
+      end
+
+      def if_condition(validator)
+        validator.options[:if]
+      end
+
+      def can_handle_condition?(condition)
+        condition.is_a?(Symbol)
+      end
+
+      def cant_handle_conditions?(validator)
+        !can_handle_condition?(if_condition(validator)) && !can_handle_condition?(unless_condition(validator))
       end
     end
   end
